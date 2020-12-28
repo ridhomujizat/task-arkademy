0f22973645bf458e2ceac0dc15be20f9275ce875
ridhomujizat <ridhomujizat@gmail.com>
2020-12-28 13:34:19 +0700

    Promise update

--- task-4/3.Promise.js
+++ task-4/3.Promise.js
@@ -1,42 +1,71 @@
-const dataFilm = [
-  "Batmen",
-  "Sepiderman",
-  "Cinta Fitri",
-  "Tuhan kenapa aku berbeda",
-];
-
-const cekFilm = (film) => {
-  return new Promise((resolve, reject) => {
+function antrian(nomerAntrian) {
+  console.log(`Sekarang antrian ke ${nomerAntrian}`);
+  return new Promise(function (resolve, reject) {
     setTimeout(() => {
-      let cek = dataFilm.find((item) => {
-        return item === film;
-      });
-      if (cek) {
-        resolve(cek);
+      if (nomerAntrian === 10) {
+        console.log("saya masuk ruangan");
+        reject(0);
       } else {
-        reject(new Error("Tidak ada gilm"));
+        console.log("saya masih menunggu");
+        resolve(nomerAntrian + 1);
       }
-    }, 1000);
+    }, 500);
   });
-};
+}
 
-dataFilm.forEach((item) =>
-  cekFilm(item)
-    .then((result) => {
-      console.log(`saya menonton ${result}`);
+function RunAntrian(nomorAntrian) {
+  antrian(nomorAntrian)
+    .then(function (nomorAntrainBaru) {
+      if (nomorAntrainBaru !== 0) {
+        RunAntrian(nomorAntrainBaru);
+      }
     })
     .catch((error) => {
       throw error;
-    })
-);
-
-async function getData(film) {
-  try {
-    const result = await cekFilm(film);
-    console.log(`saya menonton ${result}`);
-  } catch (error) {
-    throw error;
-  }
+    });
 }
 
-dataFilm.forEach((item) => getData(item));
+RunAntrian(7);
+
+// const dataFilm = [
+//   "Batmen",
+//   "Sepiderman",
+//   "Cinta Fitri",
+//   "Tuhan kenapa aku berbeda",
+// ];
+
+// const cekFilm = (film) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       let cek = dataFilm.find((item) => {
+//         return item === film;
+//       });
+//       if (cek) {
+//         resolve(cek);
+//       } else {
+//         reject(new Error("Tidak ada film"));
+//       }
+//     }, 1000);
+//   });
+// };
+
+// dataFilm.forEach((item) =>
+//   cekFilm(item)
+//     .then((result) => {
+//       console.log(`saya menonton ${result}`);
+//     })
+//     .catch((error) => {
+//       throw error;
+//     })
+// );
+
+// async function getData(film) {
+//   try {
+//     const result = await cekFilm(film);
+//     console.log(`saya menonton ${result}`);
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+// dataFilm.forEach((item) => getData(item));
